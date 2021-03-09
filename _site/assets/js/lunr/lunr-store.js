6b02d1f9325e05dd7dc44bd11212dbfb650c19d8
Jaylen N <jaylen.nav@gmail.com>
2021-03-09 18:44:46 +1100

    site should be ignored

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -2,11 +2,59 @@ var store = [{
         "title": "How you can use the power of your PC to help the fight against COVID-19",
         "excerpt":"All about Folding@Home. Photo by Christian Wiediger on Unsplash During these times of crisis with the coronavirus, it is imperative for society to band together and help each other to get through this. One way you can help is with Folding@Home . A quick intro to Folding@Home- it was first introduced in...","categories": ["covid19"],
         "tags": [],
-        "url": "/covid19/2021/03/07/power-of-pc-for-covid.html",
+        "url": "/covid19/2020/04/07/power-of-pc-for-covid.html",
+        "teaser": null
+      },{
+        "title": "An iPhone is now cheaper than a OnePlus phone — here’s why the Pixel 4a is going to have a hard time",
+        "excerpt":"Apple has just recently announced the return of the iPhone SE, starting at USD$399, a model that was first introduced back in 2016, it only saw one generation until a few days ago, when Apple came out with the second — gen iPhone SE. Image Source: Apple And only a few days before...","categories": ["smartphones"],
+        "tags": [],
+        "url": "/smartphones/2020/04/21/iphone-cheap-oneplus.html",
+        "teaser": null
+      },{
+        "title": "Notion and the Value of Delight",
+        "excerpt":"Over the past few months, I’ve been using a notetaking app called Notion, and it’s a pretty great software. It’s jam-packed with features and I personally found it to be really flexible whether it came to structuring things or putting my thoughts down into Notion, I can always find a...","categories": ["productivity"],
+        "tags": [],
+        "url": "/productivity/2020/04/24/notion-and-value-delight.html",
+        "teaser": null
+      },{
+        "title": "The iPad Pro — a viable laptop replacement?",
+        "excerpt":"“Is there room for a third category of device in the middle? Something that’s between a laptop and a smartphone.” — Steve Jobs In January 2010, Steve Jobs announced a device that would impact the computing and portable technology space forever- the iPad. Introduced as an offering in between the iPhone and...","categories": ["consumertech"],
+        "tags": [],
+        "url": "/consumertech/2020/06/30/ipad-pro-viable.html",
+        "teaser": null
+      },{
+        "title": "Why do we have such an aversion to buying paid apps?",
+        "excerpt":"As someone who is interested in technology, I’ve noticed that in videos that demo an app or service, often times the first couple of comments are “is it free?” or “how about a free version?”. It’s interesting to think that many of us would have no issue with spending 3–4$...","categories": ["consumertech"],
+        "tags": [],
+        "url": "/consumertech/2020/07/01/aversion-to-paid-apps.html",
+        "teaser": null
+      },{
+        "title": "5 Digital Tools to Help You Be More Productive",
+        "excerpt":"Behind every productive person is a system — one that is able to carry thoughts and ideas, and one that is able to be a person’s ‘second brain’. Note: I am not saying that these apps will suddenly multiply your productivity, but they certainly can help you get more things done. For...","categories": ["productivity"],
+        "tags": [],
+        "url": "/productivity/2020/07/02/5-digital-tools-productivity.html",
+        "teaser": null
+      },{
+        "title": "OnePlus returning to their roots with the OnePlus Nord",
+        "excerpt":"Ahh, OnePlus. The self proclaimed “flagship-killer”. The brand that undercut the competition so severely that it attracted a large amount of hype, got a massive amount of traction and here they are today, 15 phones later. But over the years, the prices of OnePlus phones have been going up and...","categories": [],
+        "tags": [],
+        "url": "/2020/07/02/oneplus-return-roots.html",
+        "teaser": null
+      },{
+        "title": "Ratio, A New Minimalist Launcher",
+        "excerpt":"Blloc — it’s a company that you probably haven’t heard of before. They’re a Berlin based startup, who made a phone called the Blloc Zero 18, a phone that they marketed as the “minimalist smartphone”. It was a pretty average midrange phone, with a design that was reminiscent of the iPhone X...","categories": ["smartphones"],
+        "tags": [],
+        "url": "/smartphones/2020/07/07/a-new-minimalist-launcher.html",
+        "teaser": null
+      },{
+        "title": "TikTok — Powerful for Marketing?",
+        "excerpt":"TikTok- the platform once popularly known as Musical.ly, rebranded to TikTok in August of 2018, has been taking over the app stores, with the app itself having over 800 million active users. Its demographic is primarily 16–24-year-olds, and with a million videos viewed per day, its clear to see why...","categories": ["socialmedia"],
+        "tags": [],
+        "url": "/socialmedia/2020/07/09/tiktok-powerful-for-marketing.html",
         "teaser": null
       },{
         "title": "What the hell are NFTs?",
-        "excerpt":"A new type of digital art that’s blowing up Chris Torres’ Nyan Cat meme which was sold as an NFT for $590,000. By now you’ve probably heard about this cool new thing called NFTs that are absolutely blowing up. Seemingly absurd amounts of money are being paid for all forms of...","categories": ["crypto"],
+        "excerpt":"By now you’ve probably heard about this cool new thing called NFTs that are absolutely blowing up. Seemingly absurd amounts of money are being paid for all forms of digital art: JPEGs, GIFs, and more. So what are NFTs? NFT stands for ‘non-fungible token’ — something being fungible means that it can...","categories": ["crypto"],
         "tags": [],
         "url": "/crypto/2021/03/07/what-the-hell-are-nfts.html",
         "teaser": null
